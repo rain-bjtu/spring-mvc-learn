32aca23a1d8aee21451c724453579f2ba0911efb
rain-bjtu <12120351@bjtu.edu.cn>
2016-11-08 00:58:35 +0800

    add logger
    store.log output to ~/logs/ folder, need to check why?

--- src/main/java/com/rain/learn/sms/controller/IndexController.java
+++ src/main/java/com/rain/learn/sms/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.rain.learn.sms.controller;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController extends BaseController {
+    
+    private static Logger logger = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String showLogin(ModelMap model) {
+        logger.debug("request index");
         return "/index";
     }
 }
